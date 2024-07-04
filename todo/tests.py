642100eee12f440f7b95109c2e97a51cf6a39001
bessho <bessho@iniad.org>
2024-07-04 11:24:49 +0900

    コード整形

--- todo/tests.py
+++ todo/tests.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
@@ -44,14 +45,15 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(task.is_overdue(current))
-        
+
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title='task1')
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-        
+
+
 class TodoViewsTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -83,7 +85,7 @@ class TodoViewsTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
-        
+
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
@@ -97,5 +99,3 @@ class TodoViewsTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
-        
-    
